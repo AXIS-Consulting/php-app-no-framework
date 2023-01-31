b6392c36d5e7b84cd159a6744cc6da2dcac2cdaf
Aaron Curtis <106179434+AXIS-Consulting@users.noreply.github.com>
2023-01-31 12:31:06 -0500

    Update head.php
    removed base url

--- views/partials/head.php
+++ views/partials/head.php
@@ -2,7 +2,6 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <base href="https://axisac.com/projects/php-app-no-framework/public/" />
     <title>PHP Demo</title>
     <link rel="stylesheet" href="/css/style.css">
 </head>
